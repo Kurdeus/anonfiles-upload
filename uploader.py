0648774172137d97f91f1954263b07ec6bb09981
Kurdeus <111803918+Kurdeus@users.noreply.github.com>
2022-08-25 18:00:07 +0430

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,21 +1,27 @@
 import requests, html
 
+class anonfiles():
+    def __init__(self):
+        self.drive_service = self.service()
+
+    def dl(self, url):
+        r = requests.get(url).content
+        tree = html.fromstring(r)
+        dlink = tree.xpath('//a[@class="btn btn-primary btn-block"]/@href')
+        return str(dlink[0]).replace(' ','%20')
 
 
-def direct(i):
-    r = requests.get(i).content
-    tree = html.fromstring(r)
-    dlink = tree.xpath('//a[@class="btn btn-primary btn-block"]/@href')
-    return str(dlink[0]).replace(' ','%20')
+    def upload(self, file):
+        callapi = requests.post("https://api.anonfiles.com/upload", 
+        files={'file': open(file, 'rb')}).json()
+        url = (callapi['data']['file']['url']['full'])
+        return url
+
+path = 'YOUR_FILE_PATH'
+
+af = anonfiles()
+anon_link = af.upload(path)
+direct_link = af.dl(anon_link)
 
 
-def upload(file):
-    callapi = requests.post("https://api.anonfiles.com/upload", 
-    files={'file': open(file, 'rb')}).json()
-    url = (callapi['data']['file']['url']['full'])
-    return [url, direct(url)]
-
-
-
-output = upload('your_file')
-print(output)
+print(anon_link, direct_link)
